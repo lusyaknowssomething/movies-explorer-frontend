ffbb497b0daf498bcce80048f618414b85540a56
Lyudmila Khatanzeyskaya <lyudmila.khatanzeyskaya@yandex.ru>
2022-06-09 15:38:18 +0300

    add About Project

--- src/scripts/components/AboutProject/AboutProject.js
+++ src/scripts/components/AboutProject/AboutProject.js
@@ -1,12 +1,47 @@
 import React from "react";
+import SectionTitle from "../SectionTitle/SectionTitle";
+import "./AboutProject.css";
 
 function AboutProject() {
-
   return (
-    <header className="about-project">
-      <h1>AboutProject</h1>
-    </header>
+    <section className="section about-project">
+      <SectionTitle>О проекте</SectionTitle>
+      <article className="about-project__article">
+        <section className="about-project__section">
+          <h3 className="about-project__section-title">
+            Дипломный проект включал 5&nbsp;этапов
+          </h3>
+          <p className="about-project__section-text">
+            Составление плана, работу над бэкендом, вёрстку, добавление
+            функциональности и&nbsp;финальные доработки.
+          </p>
+        </section>
+        <section className="about-project__section">
+          <h3 className="about-project__section-title">
+            На&nbsp;выполнение диплома ушло 5&nbsp;недель
+          </h3>
+          <p className="about-project__section-text">
+            У&nbsp;каждого этапа был мягкий и&nbsp;жёсткий дедлайн, которые
+            нужно было соблюдать,&nbsp;чтобы успешно защититься.
+          </p>
+        </section>
+      </article>
+      <div className="scale">
+        <div className="scale__section">
+          <p className="scale__text">1 неделя</p>
+        </div>
+        <div className="scale__section">
+          <p className="scale__text">4 недели</p>
+        </div>
+        <div className="scale__section">
+          <p className="scale__under-text">Back-end</p>
+        </div>
+        <div className="scale__section">
+          <p className="scale__under-text">Front-end</p>
+        </div>
+      </div>
+    </section>
   );
-};
+}
 
 export default AboutProject;
---
+++ src/scripts/components/SectionTitle/SectionTitle.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./SectionTitle.css";
+
+const SectionTitle = (props) => {
+  return (
+    <h2 className="section__title">
+      {props.children}
+    </h2>
+  )
+}
+
+export default SectionTitle;
