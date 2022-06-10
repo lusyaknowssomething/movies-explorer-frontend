0094f64971f4618322d62a09f01d6f884bc53989
Lyudmila Khatanzeyskaya <lyudmila.khatanzeyskaya@yandex.ru>
2022-06-10 23:30:32 +0300

    add Portfolio block

--- src/scripts/components/Portfolio/Portfolio.js
+++ src/scripts/components/Portfolio/Portfolio.js
@@ -1,15 +1,54 @@
 import React from "react";
+import ArrowSvg from "../../../images/arrow.svg";
+import "./Portfolio.css";
 
 function Portfolio() {
-
   return (
-    <section>
-
-      <article>
-        <h3 className="portfolio">Портфолио</h3>
-      </article>
+    <section className="portfolio portfolio__page">
+      <h3 className="portfolio__title">Портфолио</h3>
+      <ul className="portfolio__list">
+        <li className="portfolio__item">
+          <a
+            className="portfolio__link"
+            href="https://github.com/lusyaknowssomething/how-to-learn"
+          >
+            Статичный сайт
+          </a>
+          <img
+            className="portfolio__icon"
+            src={ArrowSvg}
+            alt="иконка стрелочка"
+          />
+        </li>
+        <li className="portfolio__item">
+          <a
+            className="portfolio__link"
+            href="https://github.com/lusyaknowssomething/russian-travel"
+          >
+            Адаптивный сайт
+          </a>
+          <img
+            className="portfolio__icon"
+            src={ArrowSvg}
+            alt="иконка стрелочка"
+          />
+        </li>
+        <li className="portfolio__item">
+          <a
+            className="portfolio__link"
+            href="https://github.com/lusyaknowssomething/react-mesto-api-full"
+          >
+            Одностраничное приложение
+          </a>
+          <img
+            className="portfolio__icon"
+            src={ArrowSvg}
+            alt="иконка стрелочка"
+          />
+        </li>
+      </ul>
     </section>
   );
-};
+}
 
 export default Portfolio;
