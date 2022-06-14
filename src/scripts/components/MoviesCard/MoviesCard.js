a0d3da10eacc55e52c2f2164092d953ef811f5c2
Lyudmila Khatanzeyskaya <lyudmila.khatanzeyskaya@yandex.ru>
2022-06-14 20:00:26 +0300

    fix after review

--- src/scripts/components/MoviesCard/MoviesCard.js
+++ src/scripts/components/MoviesCard/MoviesCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './MoviesCard.css';
+import { useLocation } from 'react-router-dom';
 
 function MoviesCard({ name, time, img}) {
   // Определяем, сохранена ли карточка
@@ -10,14 +11,26 @@ function MoviesCard({ name, time, img}) {
     isSaved ? "element__btn_active" : ""
   }`;
 
+  const location = useLocation();
+  const savedMoviesPath = ['/saved-movies'].includes(location.pathname);
+
   return (
     <article className="element">
       <h2 className="element__title">{name}</h2>
       <p className="element__time">{time}</p>
-      <button
-        type="button"
-        className={cardLikeButtonClassName}
-      ></button>
+      {savedMoviesPath ? (
+        <button
+          type="button"
+          className="element__btn element__btn_delete"
+        >
+        </button>
+      ) : (
+        <button
+          type="button"
+          className={cardLikeButtonClassName}
+        >
+        </button>
+      )}
       <div className="element__pic-container">
         <img
           src={img}
